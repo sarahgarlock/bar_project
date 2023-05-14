80b1bdb187c5fd6a4c20bb567ab040ee57e3b5ed
Sarah Garlock <garlock.sarah10@gmail.com>
2023-05-13 20:01:26 -0600

    change beer type

--- db/migrate/20230511184433_create_kegs.rb
+++ db/migrate/20230511184433_create_kegs.rb
@@ -2,7 +2,7 @@ class CreateKegs < ActiveRecord::Migration[7.0]
   def change
     create_table :kegs do |t|
       t.string :name
-      t.string :type
+      t.string :beer_type
       t.boolean :ordered
       t.integer :abv
       t.integer :ibu
