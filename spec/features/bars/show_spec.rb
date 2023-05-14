b1dc76b94239b94b11e3b7ac6756ed5eaa0b2760
Sarah Garlock <garlock.sarah10@gmail.com>
2023-05-13 20:03:56 -0600

    add show spec to bar

--- spec/features/bars/show_spec.rb
+++ spec/features/bars/show_spec.rb
@@ -1,18 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "/bars/:id", type: :feature do
-  describe "as a visitor, when I visit a Bar show page" do
-    # let(:jacks_bar) { Bar.create!(name: "Jacks Bar", city: "Arvada", tap_quantity: 8, overnight_delivery: false) }
-    # let(:@crow_bar) { Bar.create!(name: "Crow Bar", city: "Wheat Ridge", tap_quantity: 10, overnight_delivery: false) }
-    # User Story 2
-    it "will show the bar with that id including the bar's attributes" do
-      jacks_bar = Bar.create!(name: "Jacks Bar", city: "AAArvada", tap_quantity: 8, overnight_delivery: false)
-      visit "/bars/:id"
+RSpec.describe "/bars/:id" do
+  describe "As a visitor, when I visit /bars/:id"
+    it "will display the bar attributes" do
+      jacks_bar = Bar.create!(name: "Jacks Bar", city: "Arvada", tap_quantity: 8, overnight_delivery: false)
+      visit "/bars/#{jacks_bar.id}"
+      save_and_open_page
 
-      expect(page).to have_content("Name: #{jacks_bar.name}")
-      expect(page).to have_content("City: #{jacks_bar.city}")
-      expect(page).to have_content("Tap Quantity: #{jacks_bar.tap_quantity}")
-      expect(page).to have_content("Overnight Delivery: #{jacks_bar.overnight_delivery}")
+      expect(page).to have_content(jacks_bar.name)
+      expect(page).to have_content(jacks_bar.city)
+      expect(page).to have_content(jacks_bar.tap_quantity)
+      expect(page).to have_content(jacks_bar.overnight_delivery)
     end
-  end
 end
