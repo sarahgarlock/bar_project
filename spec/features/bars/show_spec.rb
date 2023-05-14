cc98501f0ce701f7b2ff1be9fdff484b27922e4c
Sarah Garlock <garlock.sarah10@gmail.com>
2023-05-14 16:04:57 -0600

    add kegs to parent id

--- spec/features/bars/show_spec.rb
+++ spec/features/bars/show_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe "/bars/:id" do
   describe "As a visitor, when I visit /bars/:id"
     it "will display the bar attributes" do
       jacks_bar = Bar.create!(name: "Jacks Bar", city: "Arvada", tap_quantity: 8, overnight_delivery: false)
+      keg1 = jacks_bar.kegs.create!(name: "Fat Tire", beer_type: "Amber", ordered: false, abv: 5, ibu: 22, price: 140)
+      keg2 = jacks_bar.kegs.create!(name: "VooDoo Ranger Juicy Haze", beer_type: "Hazy IPA", ordered: true, abv: 7, ibu: 41, price: 155, bar_id: jacks_bar.id)
+
       visit "/bars/#{jacks_bar.id}"
       save_and_open_page
 
