1d43cf562f847b0745d9bab545aa654eb5978dcf
Sarah Garlock <garlock.sarah10@gmail.com>
2023-05-14 20:12:14 -0600

    organize all pages and add user story comments

--- spec/features/bars/show_spec.rb
+++ spec/features/bars/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "/bars/:id", type: :feature do
   before(:each) do
-    @jacks_bar = Bar.create!(name: "Jacks Bar", city: "Arvada", tap_quantity: 8, overnight_delivery: false)
+    @jacks_bar = Bar.create!(name: "Jacks Bar", city: "Arvada", tap_quantity: 8, overnight_delivery: false, created_at: Date.today)
     @crow_bar =  Bar.create!(name: "Crow Bar", city: "Wheat Ridge", tap_quantity: 10, overnight_delivery: false, created_at: Date.today)
     @keg_1 = Keg.create!(name: "Fat Tire", beer_type: "Amber", ordered: false, abv: 5, ibu: 22, price: 140, bar_id: @jacks_bar.id)
     @keg_2 = Keg.create!(name: "VooDoo Ranger Juicy Haze", beer_type: "Hazy IPA", ordered: true, abv: 7, ibu: 41, price: 155, bar_id: @jacks_bar.id)
@@ -13,13 +13,11 @@ RSpec.describe "/bars/:id", type: :feature do
     it "will display the bar attributes" do
       # User Story 2
       visit "/bars/#{@jacks_bar.id}"
-      
+    
       expect(page).to have_content(@jacks_bar.name)
       expect(page).to have_content(@jacks_bar.city)
       expect(page).to have_content(@jacks_bar.tap_quantity)
       expect(page).to have_content(@jacks_bar.overnight_delivery)
-      
-      # save_and_open_page
     end
 
     it "should count the number of kegs each bar has" do
@@ -29,29 +27,28 @@ RSpec.describe "/bars/:id", type: :feature do
 
       visit "/bars/#{@crow_bar.id}"
       expect(page).to have_content("#{@crow_bar.keg_count}")
-      # save_and_open_page
     end
   end
 
   describe "when I visit any page" do
-    it 'will display the Keg\'s index link at the top' do
+    it "will display the Keg\'s index link at the top" do
       # User Story 8
       visit "/bars/#{@jacks_bar.id}"
-      expect(page).to have_link('Keg Index Page', href: '/kegs')
+      expect(page).to have_link("Keg Index Page")
 
       click_link "Keg Index Page"
       expect(current_path).to eq("/kegs")
-      # save_and_open_page
     end
 
     it 'will display the Bar\'s index link at the top' do
       # User Story 9
       visit "/bars/#{@jacks_bar.id}"
-
       expect(page).to have_link("Bar Index Page")
+
       click_link "Bar Index Page"
       expect(current_path).to eq("/bars")
-      # save_and_open_page
+
+      save_and_open_page
     end
   end
 end
--- spec/features/kegs/show_spec.rb
+++ spec/features/kegs/show_spec.rb
@@ -2,19 +2,44 @@ require 'rails_helper'
 
 RSpec.describe "/kegs/:id" do
   describe "As a visitor, when I visit /kegs/:id" do
-    it "will display the kegs attributes" do
-      # User Story 4
-      jacks_bar = Bar.create!(name: "Jacks Bar", city: "Arvada", tap_quantity: 8, overnight_delivery: false)
-      keg2 = Keg.create!(name: "VooDoo Ranger Juicy Haze", beer_type: "Hazy IPA", ordered: true, abv: 7, ibu: 41, price: 155, bar_id: jacks_bar.id)
-      visit "/kegs/#{jacks_bar.id}"
+    before(:each) do
+      @jacks_bar = Bar.create!(name: "Jacks Bar", city: "Arvada", tap_quantity: 8, overnight_delivery: false, created_at: Date.today - 2.day)
+      @crow_bar =  Bar.create!(name: "Crow Bar", city: "Wheat Ridge", tap_quantity: 10, overnight_delivery: false, created_at: Date.today - 1.day)
+      @local_46 =  Bar.create!(name: "Local 46", city: "Wheat Ridge", tap_quantity: 15, overnight_delivery: true, created_at: Date.today)
 
-      expect(page).to have_content(keg2.name)
-      expect(page).to have_content(keg2.beer_type)
-      expect(page).to have_content(keg2.ordered)
-      expect(page).to have_content(keg2.abv)
-      expect(page).to have_content(keg2.ibu)
-      expect(page).to have_content(keg2.price)
-      # save_and_open_page
+      @keg_1 = Keg.create!(name: "Fat Tire", beer_type: "Amber", ordered: false, abv: 5, ibu: 22, price: 140, bar_id: @jacks_bar.id)
+      @keg_2 = Keg.create!(name: "VooDoo Ranger Juicy Haze", beer_type: "Hazy IPA", ordered: true, abv: 7, ibu: 41, price: 155, bar_id: @jacks_bar.id)
+      @keg_3 = Keg.create!(name: "Little Mo'Porter", beer_type: "Porter", ordered: false, abv: 6, ibu: 20, price: 165, bar_id: @crow_bar.id)
+    end
+
+    it "will display the kegs attributes with that id" do
+      # User Story 4
+      visit "/kegs/#{@keg_1.id}"
+
+      expect(page).to have_content(@keg_1.name)
+      expect(page).to have_content(@keg_1.beer_type)
+      expect(page).to have_content(@keg_1.ordered)
+      expect(page).to have_content(@keg_1.abv)
+      expect(page).to have_content(@keg_1.ibu)
+      expect(page).to have_content(@keg_1.price)
+
+      visit "/kegs/#{@keg_2.id}"
+
+      expect(page).to have_content(@keg_2.name)
+      expect(page).to have_content(@keg_2.beer_type)
+      expect(page).to have_content(@keg_2.ordered)
+      expect(page).to have_content(@keg_2.abv)
+      expect(page).to have_content(@keg_2.ibu)
+      expect(page).to have_content(@keg_2.price)
+
+      visit "/kegs/#{@keg_3.id}"
+
+      expect(page).to have_content(@keg_3.name)
+      expect(page).to have_content(@keg_3.beer_type)
+      expect(page).to have_content(@keg_3.ordered)
+      expect(page).to have_content(@keg_3.abv)
+      expect(page).to have_content(@keg_3.ibu)
+      expect(page).to have_content(@keg_3.price)
     end
   end
 end
